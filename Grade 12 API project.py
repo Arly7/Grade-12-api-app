0c50ff83a54053525bd2316bac489f0ec977cc69
Arly7 <164506095+Arly7@users.noreply.github.com>
2024-10-23 09:44:40 -0400

    Update Grade 12 API project.py

--- Grade 12 API project.py
+++ Grade 12 API project.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 import os
 
-genai.configure(api_key="AIzaSyCwg59ci5AKCJC9aDLVWM4KAsB2Pq1XfAw")
+genai.configure(api_key=)
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Make me an recipe with disgns.")
